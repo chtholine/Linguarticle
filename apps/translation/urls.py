033214836a34bd349c6112bf86853d5d9d451b01
chtholine <oleg123qwe@gmail.com>
2023-04-06 16:06:43 +0300

    🛤 added urls

--- apps/translation/urls.py
+++ apps/translation/urls.py
@@ -1,6 +1,21 @@
-from . import views
+from .views import *
 from django.urls import path
 
 urlpatterns = [
-    path("parse", views.ScrapingView.as_view(), name="parse")
+    # Auth
+    path('signup/', SignUpAPIView.as_view(), name='signup'),
+    path('login/', LoginAPIView.as_view(), name='login'),
+    path('logout/', LogoutAPIView.as_view(), name='logout'),
+
+    # Settings
+    path('settings/username/', UpdateUsernameAPIView.as_view(), name='update-username'),
+    path('settings/email/', UpdateEmailAPIView.as_view(), name='update-email'),
+    path('settings/password/', UpdatePasswordAPIView.as_view(), name='update-password'),
+
+    # Articles
+    path('articles/', ArticlesView.as_view(), name='articles'),
+    path('articles/add/', AddArticleView.as_view(), name='add-article'),
+    path('articles/<int:id>/', ShowArticleView.as_view(), name='show-article'),
+    path('articles/<int:id>/update/', UpdateArticleView.as_view(), name='update-article'),
+    path('articles/<int:id>/delete/', DeleteArticleView.as_view(), name='delete-article'),
 ]
--- core/urls.py
+++ core/urls.py
@@ -16,12 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from .yasg import urlpatterns as doc_urls
+from .token_views import MyTokenObtainPairView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/", include("apps.translation.urls")),
-    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/v1/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
