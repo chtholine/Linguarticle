de986e04f2127e93d04b589ff5b03d8a490bef83
chtholine <oleg123qwe@gmail.com>
2023-04-06 19:44:14 +0300

    🛤 added urls

--- apps/translation/urls.py
+++ apps/translation/urls.py
@@ -3,19 +3,17 @@ from django.urls import path
 
 urlpatterns = [
     # Auth
-    path('signup/', SignUpAPIView.as_view(), name='signup'),
-    path('login/', LoginAPIView.as_view(), name='login'),
-    path('logout/', LogoutAPIView.as_view(), name='logout'),
-
+    path("signup/", SignUpAPIView.as_view(), name="signup"),
+    path("login/", LoginAPIView.as_view(), name="login"),
+    path("logout/", LogoutAPIView.as_view(), name="logout"),
     # Settings
-    path('settings/username/', UpdateUsernameAPIView.as_view(), name='update-username'),
-    path('settings/email/', UpdateEmailAPIView.as_view(), name='update-email'),
-    path('settings/password/', UpdatePasswordAPIView.as_view(), name='update-password'),
-
+    path("settings/username/", UpdateUsernameAPIView.as_view(), name="update-username"),
+    path("settings/email/", UpdateEmailAPIView.as_view(), name="update-email"),
+    path("settings/password/", UpdatePasswordAPIView.as_view(), name="update-password"),
     # Articles
-    path('articles/', ArticlesView.as_view(), name='articles'),
-    path('articles/add/', AddArticleView.as_view(), name='add-article'),
-    path('articles/<int:id>/', ShowArticleView.as_view(), name='show-article'),
-    path('articles/<int:id>/update/', UpdateArticleView.as_view(), name='update-article'),
-    path('articles/<int:id>/delete/', DeleteArticleView.as_view(), name='delete-article'),
+    path("articles/", ArticlesView.as_view(), name="articles"),
+    path("articles/add/", AddArticleView.as_view(), name="add-article"),
+    path("articles/<int:id>/", ShowArticleView.as_view(), name="show-article"),
+    path("articles/<int:id>/update/", UpdateArticleView.as_view(), name="update-article"),
+    path("articles/<int:id>/delete/", DeleteArticleView.as_view(), name="delete-article"),
 ]
