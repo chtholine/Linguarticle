6576f7ab6488cb1595226f998e02f7324372c056
chtholine <oleg123qwe@gmail.com>
2023-04-30 13:26:23 +0300

    added scraping, dictionary

--- apps/translation/views.py
+++ apps/translation/views.py
@@ -1,67 +1,41 @@
-from scrapy.crawler import CrawlerProcess
-from scrapy.utils.log import configure_logging
-
-from scraper.scraper.spiders.myspider import ArticleSpider
+import os
+import nltk
+from deep_translator import GoogleTranslator
+import subprocess
+from drf_yasg import openapi
+import requests
+from bs4 import BeautifulSoup
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework_simplejwt.tokens import RefreshToken
 from django.contrib.auth import authenticate, login, logout
-from .serializers import UserSerializer
+from .serializers import UserSerializer, DictionarySerializer
 from rest_framework.views import APIView
 from rest_framework import status, permissions
-from .models import Article
+from .models import Article, Dictionary
 from .serializers import ArticleSerializer
-from django.views.decorators.http import require_POST, require_http_methods
-from scrapyd_api import ScrapydAPI
-from uuid import uuid4
-from urllib.parse import urlparse
 from django.core.validators import URLValidator
 from django.core.exceptions import ValidationError
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from twisted.internet import reactor
 from django.shortcuts import render
-from crochet import setup, wait_for
-from scrapy.crawler import CrawlerRunner
-from django.http import JsonResponse
-from django.views import View
-
-setup()  # Initialize crochet
 
 
-class StartCrawlView(View):
-    @wait_for(timeout=10.0)  # Set a timeout for the crawl operation
-    def run_crawl(self, url):
-        runner = CrawlerRunner()
-        return runner.crawl(ArticleSpider, start_url=url)
-
-    def post(self, request, *args, **kwargs):
-        url = request.POST.get('url')
-        self.run_crawl(url)  # Run the crawl operation
-        return JsonResponse({"status": "success"})
-
-
-# connect scrapyd service
-scrapyd = ScrapydAPI("http://localhost:6800")
+def url_valid(url):
+    validate = URLValidator()
+    try:
+        validate(url)
+    except ValidationError:
+        return False
+    return True
 
 
 def submit_url(request):
-    return render(request, 'add_article.html')
-
-
-def is_valid_url(url):
-    validate = URLValidator()
-    try:
-        validate(url)  # check if url format is valid
-    except ValidationError:
-        return False
-
-    return True
+    return render(request, "add_article.html")
 
 
 # --- Auth --- #
 class SignUpAPIView(APIView):
-    permission_classes = [permissions.AllowAny]
-
+    @swagger_auto_schema(request_body=UserSerializer)
     def post(self, request):
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
@@ -81,8 +55,16 @@ class SignUpAPIView(APIView):
 
 
 class LoginAPIView(APIView):
-    permission_classes = [permissions.AllowAny]
-
+    @swagger_auto_schema(
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                "username": openapi.Schema(type=openapi.TYPE_STRING),
+                "password": openapi.Schema(type=openapi.TYPE_STRING),
+            },
+            required=["username", "password"],
+        )
+    )
     def post(self, request):
         username = request.data.get("username")
         password = request.data.get("password")
@@ -114,6 +96,7 @@ class LogoutAPIView(APIView):
 class UpdateUsernameAPIView(APIView):
     permission_classes = [permissions.IsAuthenticated]
 
+    @swagger_auto_schema(request_body=UserSerializer)
     def put(self, request):
         user = request.user
         serializer = UserSerializer(user, data=request.data, partial=True)
@@ -126,6 +109,7 @@ class UpdateUsernameAPIView(APIView):
 class UpdateEmailAPIView(APIView):
     permission_classes = [permissions.IsAuthenticated]
 
+    @swagger_auto_schema(request_body=UserSerializer)
     def put(self, request):
         user = request.user
         serializer = UserSerializer(user, data=request.data, partial=True)
@@ -138,6 +122,16 @@ class UpdateEmailAPIView(APIView):
 class UpdatePasswordAPIView(APIView):
     permission_classes = [permissions.IsAuthenticated]
 
+    @swagger_auto_schema(
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                "old_password": openapi.Schema(type=openapi.TYPE_STRING),
+                "new_password": openapi.Schema(type=openapi.TYPE_STRING),
+            },
+            required=["old_password", "new_password"],
+        )
+    )
     def put(self, request):
         user = request.user
         old_password = request.data.get("old_password")
@@ -154,7 +148,7 @@ class ArticlesView(APIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def get(self, request):
-        articles = Article.objects.filter(user=request.user)
+        articles = Article.objects.filter(user=request.user).order_by("-date_added")
         serializer = ArticleSerializer(articles, many=True)
         return Response(serializer.data)
 
@@ -162,12 +156,40 @@ class ArticlesView(APIView):
 class AddArticleView(APIView):
     permission_classes = [permissions.IsAuthenticated]
 
+    @swagger_auto_schema(
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT, properties={"url": openapi.Schema(type=openapi.TYPE_STRING)}, required=["url"]
+        )
+    )
     def post(self, request):
-        serializer = ArticleSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save(user=request.user)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        # canonical url check
+        url = request.data.get("url")
+        if url is None:
+            return Response({"error": "URL parameter is missing."}, status=400)
+        if not url_valid(url):
+            return Response({"error": "URL is invalid."}, status=400)
+        html = requests.get(url).text
+        soup = BeautifulSoup(html, "html.parser")
+        link_tag = soup.find("link", rel="canonical")
+        canonical_url = link_tag["href"] if link_tag is not None else url
+        if Article.objects.filter(url=canonical_url).exists():
+            article = Article.objects.get(url=canonical_url)
+            article.user.add(request.user)
+            article.save()
+            return Response({"error": "Existing article is added to the user."}, status=400)
+
+        # launch scrapy spider with subprocess
+        os.environ["PYTHONPATH"] = "/home/chtholine/PycharmProjects/django_translation/scraper"
+        spider_name = "article_spider"
+        user_id = request.user.id
+        command = f"scrapy crawl {spider_name} -a url={canonical_url} -a user_id={user_id}"
+        try:
+            subprocess.run(
+                command.split(), check=True, cwd="/home/chtholine/PycharmProjects/django_translation/scraper"
+            )
+            return Response({"message": f"{spider_name} spider started for URL: {canonical_url}"})
+        except subprocess.CalledProcessError as e:
+            return Response({"error": str(e)}, status=500)
 
 
 class ShowArticleView(APIView):
@@ -182,6 +204,7 @@ class ShowArticleView(APIView):
 class UpdateArticleView(APIView):
     permission_classes = [permissions.IsAuthenticated]
 
+    @swagger_auto_schema(request_body=ArticleSerializer)
     def put(self, request, id):
         article = Article.objects.get(id=id, user=request.user)
         serializer = ArticleSerializer(article, data=request.data, partial=True)
@@ -196,5 +219,57 @@ class DeleteArticleView(APIView):
 
     def delete(self, request, id):
         article = Article.objects.get(id=id, user=request.user)
-        article.delete()
-        return Response({"message": f"Article '{article.title}' deleted successfully."})
+        article.user.remove(request.user)
+        return Response({"message": f"Article '{article.title}' removed successfully."})
+
+
+# -- Dictionary -- #
+class DictionaryView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        dictionary = Dictionary.objects.filter(user=request.user).order_by("-date_added")
+        serializer = DictionarySerializer(dictionary, many=True)
+        return Response(serializer.data)
+
+
+class AddWordView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    # authentication_classes = [JWTAuthentication]
+
+    @swagger_auto_schema(
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                "word": openapi.Schema(type=openapi.TYPE_STRING),
+            },
+            required=["word"],
+        ),
+    )
+    def post(self, request):
+        word = request.data.get("word")
+        translation = GoogleTranslator(source="auto", target="uk").translate(word)
+        dictionary = Dictionary(word=word, translation=translation)
+        dictionary.save()
+        dictionary.user.add(request.user)  # Associate the word with the current user
+        serializer = DictionarySerializer(dictionary)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class ShowWordView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request, id):
+        dictionary = Dictionary.objects.get(id=id, user=request.user)
+        serializer = DictionarySerializer(dictionary)
+        return Response(serializer.data)
+
+
+class DeleteWordView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def delete(self, request, id):
+        dictionary = Dictionary.objects.get(id=id, user=request.user)
+        dictionary.user.remove(request.user)
+        return Response({"message": f"Word '{dictionary.word}' removed successfully."})
