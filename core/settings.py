62787f5239ad53dc42e19944769461f61423fed8
chtholine <oleg123qwe@gmail.com>
2023-04-30 13:26:50 +0300

    django settings

--- core/settings.py
+++ core/settings.py
@@ -16,6 +16,7 @@ import environ
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 APPS_DIR = BASE_DIR.joinpath("apps")
+SCRAPY_DIR = BASE_DIR.joinpath("scraper")
 
 env = environ.Env()
 env.read_env(BASE_DIR.joinpath(".env"))
