f384ba30fadf11e4246ceb2fe4ba9393bab2ee87
chtholine <oleg123qwe@gmail.com>
2023-04-30 13:29:45 +0300

    scrapy to db pipeline

--- scraper/scraper/pipelines.py
+++ scraper/scraper/pipelines.py
@@ -1,16 +1,20 @@
-from asgiref.sync import sync_to_async
-from django.db import connections
-from django.db.utils import OperationalError
-from itemadapter import ItemAdapter
 from scrapy.exceptions import DropItem
 from apps.translation.models import Article
 
+
 class ScraperPipeline:
     def process_item(self, item, spider):
-        article = Article()
-        article.url = item['url']
-        article.title = item['title']
-        article.author = item['author']
-        article.data = item['data']
+        url = item["url"]
+        if Article.objects.filter(url=url).exists():
+            article = Article.objects.get(url=url)
+            article.user.add(spider.user_id)
+            article.save()
+            raise DropItem(f"Existing article found. User added to the article: {url}")
+
+        article = Article.objects.create(url=url)
+        article.user.add(spider.user_id)
+        article.title = item["title"]
+        article.author = item["author"]
+        article.data = item["data"]
         article.save()
         return item
